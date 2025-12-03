f913eb1e42303fe170954b81ab762445126c19e6
= <=>
2025-12-03 16:22:55 -0300

    teste automatico para nao ficar fazendo um por um no postman

---
+++ src/tests/exemplo.test.ts
@@ -0,0 +1,11 @@
+//todo esse metodo serve para fazer o teste de uma so ves,
+//  ao inves de ir no postman testar um por um 
+test("Get: /tarefas = 200", async() =>{
+    const res = await fetch("http://localhost:3000/tarefas/")
+    expect(res.status).toBe(200)
+
+    const body = await res.json()
+    expect(Array.isArray(body)).toBe(true)
+})
+
+
